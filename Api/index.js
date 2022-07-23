919fcadd5335169c585b3c147056331eb759f63d
root <root@VICTOR-LOPES-PC.localdomain>
2022-07-23 04:36:21 -0300

    refactor git and index structure

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Octokit } = require('@octokit/rest');
+require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,88 +16,25 @@ function compareDates(a,b) {
   return new Date(a.created_at) - new Date(b.created_at);
 }
 
-app.get('/a', async(_req, res) => {
+app.get('/repos/:repo', async(req, res) => {
   const githubApiResponse = await octokit.request('GET /search/repositories', {
     q: "org:takenet language:c#",
   })
+
+  const { repo } = req.params;
 
   const githubProjects = githubApiResponse.data.items
   const sortedProjects = githubProjects.sort(compareDates)
 
-  let slicedProjects = sortedProjects.slice(0, 4)
-  
-  res.status(200).send(slicedProjects[0])
+  res.status(200).send(sortedProjects[repo])
 });
 
-app.get('/b', async(_req, res) => {
-  const githubApiResponse = await octokit.request('GET /search/repositories', {
-    q: "org:takenet language:c#",
-  })
-
-  const githubProjects = githubApiResponse.data.items
-  const sortedProjects = githubProjects.sort(compareDates)
-
-  let slicedProjects = sortedProjects.slice(0, 4)
-  
-  res.status(200).send(slicedProjects[1])
-});
-
-app.get('/c', async(_req, res) => {
-  const githubApiResponse = await octokit.request('GET /search/repositories', {
-    q: "org:takenet language:c#",
-  })
-
-  const githubProjects = githubApiResponse.data.items
-  const sortedProjects = githubProjects.sort(compareDates)
-
-  let slicedProjects = sortedProjects.slice(0, 4)
-  
-  res.status(200).send(slicedProjects[2])
-});
-
-app.get('/d', async(_req, res) => {
-  const githubApiResponse = await octokit.request('GET /search/repositories', {
-    q: "org:takenet language:c#",
-  })
-
-  const githubProjects = githubApiResponse.data.items
-  const sortedProjects = githubProjects.sort(compareDates)
-
-  let slicedProjects = sortedProjects.slice(0, 4)
-  
-  res.status(200).send(slicedProjects[3])
-});
-
-app.get('/e', async(_req, res) => {
-  const githubApiResponse = await octokit.request('GET /search/repositories', {
-    q: "org:takenet language:c#",
-  })
-
-  const githubProjects = githubApiResponse.data.items
-  const sortedProjects = githubProjects.sort(compareDates)
-
-  let slicedProjects = sortedProjects.slice(0, 4)
-  
-  res.status(200).send(slicedProjects[4])
-});
-
-app.get('/f', async(_req, res) => {
-  const githubApiResponse = await octokit.request('GET /search/repositories', {
-    q: "org:takenet language:c#",
-  })
-
-  const result2 = await octokit.request('GET /users/{username}', {
+app.get('/image-url', async(_req, res) => {
+  const githubApiResponse = await octokit.request('GET /users/{username}', {
     username: "takenet"
   })
 
-  const githubProjects = githubApiResponse.data.items
-  const sortedProjects = githubProjects.sort(compareDates)
-
-  let slicedProjects = sortedProjects.slice(0, 4)
-  
-  c.push({avatar_url: result2.data.avatar_url})
-
-  res.status(200).send(c[5].avatar_url)
+  res.status(200).send(githubApiResponse.data.avatar_url)
 });
 
 app.listen(port, () => {
