bfc1119baad466282abbaa32184c11bda24fb659
root <root@VICTOR-LOPES-PC.localdomain>
2022-07-23 01:59:00 -0300

    third commit

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const { Octokit } = require('@octokit/rest');
 const app = express();
 app.use(bodyParser.json());
 
-
-app.get('/', async(req, res) => {
+app.get('/a', async(req, res) => {
   const octokit = new Octokit({
     auth: ''
   })
@@ -30,10 +29,139 @@ app.get('/', async(req, res) => {
   
   c.push({avatar_url: result2.data.avatar_url})
 
-  res.status(200).json({res: c})
-
+  res.status(200).json({res: c[0]})
 });
 
-app.listen(process.env.PORT, () => {
+app.get('/b', async(req, res) => {
+  const octokit = new Octokit({
+    auth: ''
+  })
+
+  const result = await octokit.request('GET /search/repositories', {
+    q: "org:takenet language:c#",
+  })
+
+  const result2 = await octokit.request('GET /users/{username}', {
+    username: "takenet"
+  })
+
+  function compare(a,b) {
+    return new Date(a.created_at) - new Date(b.created_at);
+}
+
+  const RESULTADO = result.data.items.sort(compare)
+
+  let c = RESULTADO.slice(0, 5)
+  
+  c.push({avatar_url: result2.data.avatar_url})
+
+  res.status(200).json({res: c[1]})
+});
+
+app.get('/c', async(req, res) => {
+  const octokit = new Octokit({
+    auth: ''
+  })
+
+  const result = await octokit.request('GET /search/repositories', {
+    q: "org:takenet language:c#",
+  })
+
+  const result2 = await octokit.request('GET /users/{username}', {
+    username: "takenet"
+  })
+
+  function compare(a,b) {
+    return new Date(a.created_at) - new Date(b.created_at);
+}
+
+  const RESULTADO = result.data.items.sort(compare)
+
+  let c = RESULTADO.slice(0, 5)
+  
+  c.push({avatar_url: result2.data.avatar_url})
+
+  res.status(200).json({res: c[2]})
+});
+
+app.get('/d', async(req, res) => {
+  const octokit = new Octokit({
+    auth: ''
+  })
+
+  const result = await octokit.request('GET /search/repositories', {
+    q: "org:takenet language:c#",
+  })
+
+  const result2 = await octokit.request('GET /users/{username}', {
+    username: "takenet"
+  })
+
+  function compare(a,b) {
+    return new Date(a.created_at) - new Date(b.created_at);
+}
+
+  const RESULTADO = result.data.items.sort(compare)
+
+  let c = RESULTADO.slice(0, 5)
+  
+  c.push({avatar_url: result2.data.avatar_url})
+
+  res.status(200).json({res: c[3]})
+});
+
+app.get('/e', async(req, res) => {
+  const octokit = new Octokit({
+    auth: ''
+  })
+
+  const result = await octokit.request('GET /search/repositories', {
+    q: "org:takenet language:c#",
+  })
+
+  const result2 = await octokit.request('GET /users/{username}', {
+    username: "takenet"
+  })
+
+  function compare(a,b) {
+    return new Date(a.created_at) - new Date(b.created_at);
+}
+
+  const RESULTADO = result.data.items.sort(compare)
+
+  let c = RESULTADO.slice(0, 5)
+  
+  c.push({avatar_url: result2.data.avatar_url})
+
+  res.status(200).json({res: c[4]})
+});
+
+app.get('/f', async(req, res) => {
+  const octokit = new Octokit({
+    auth: ''
+  })
+
+  const result = await octokit.request('GET /search/repositories', {
+    q: "org:takenet language:c#",
+  })
+
+  const result2 = await octokit.request('GET /users/{username}', {
+    username: "takenet"
+  })
+
+  function compare(a,b) {
+    return new Date(a.created_at) - new Date(b.created_at);
+}
+
+  const RESULTADO = result.data.items.sort(compare)
+
+  let c = RESULTADO.slice(0, 5)
+  
+  c.push({avatar_url: result2.data.avatar_url})
+
+  res.status(200).send(c[5].avatar_url)
+});
+
+app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta ');
 });
